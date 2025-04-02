30ffa3b0d8421fca2cfc30ba0f0b99690d808a20
Nedeljko <nedeljkodanilovic@gmail.com>
2025-04-02 09:24:28 +0200

    First stable animation

---
+++ n-design/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        sweep: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+      animation: {
+        sweep: 'sweep 2s ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [],
+}
